6a6abe5eb2c581816319d860d48494cd41f2dea9
Philip Trevino <philt5252@gmail.com>
2015-01-22 21:43:25 -0600

    Started ProductRepository

---
+++ StoreApp/StoreApp.Core.DataAccess/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using StoreApp.Foundation.DataAccess;
+using StoreApp.Foundation.Models;
+
+namespace StoreApp.Core.DataAccess
+{
+    public class ProductRepository :IProductRepository
+    {
+        public IProduct[] All()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IProduct Find(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(IProduct product)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save(IProduct product)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ StoreApp/StoreApp.Foundation.DataAccess/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using StoreApp.Foundation.Models;
+
+namespace StoreApp.Foundation.DataAccess
+{
+    public interface IProductRepository
+    {
+        IProduct[] All();
+
+        IProduct Find(int id);
+
+        void Save(IProduct product);
+
+        void Delete(IProduct product);
+    }
+}
