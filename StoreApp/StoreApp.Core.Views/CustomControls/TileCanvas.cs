e77c9ec3e91cdc061fa092ae69d6cfa1b912f86b
mregan59 <mregan59@gmail.com>
2015-01-24 22:45:13 -0600

    theme and background

---
+++ StoreApp/StoreApp.Core.Views/CustomControls/TileCanvas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace StoreApp.Core.Views
+{
+    public class TileCanvas : Canvas
+    {
+        public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(TileCanvas), new UIPropertyMetadata(null, OnImageSourceChanged));
+
+        private static void OnImageSourceChanged(DependencyObject o, DependencyPropertyChangedEventArgs args)
+        {
+            ((TileCanvas)o).Rebuild();
+        }
+
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            base.OnRenderSizeChanged(sizeInfo);
+            Rebuild();
+        }
+
+        private void Rebuild()
+        {
+            if (ImageSource == null)
+            {
+                return;
+            }
+
+            var size = RenderSize;
+            Children.Clear();
+            var width = (int)ImageSource.Width;
+            var height = (int)ImageSource.Height;
+            for (int x = 0; x < size.Width; x += width)
+            {
+                for (int y = 0; y < size.Height; y += height)
+                {
+                    var image = new Image { Source = ImageSource };
+                    Canvas.SetLeft(image, x);
+                    Canvas.SetTop(image, y);
+                    Children.Add(image);
+                }
+            }
+            Clip = new RectangleGeometry(new Rect(size));
+        }
+    }
+}
