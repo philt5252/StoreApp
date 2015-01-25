bd77d5892df87ce4c492074bc00ff29c37e5cc7b
mregan59 <mregan59@gmail.com>
2015-01-25 16:31:54 -0600

    BookView

---
+++ StoreApp/StoreApp.Core.Views/Factories/BookViewFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using StoreApp.Core.Views.Views;
+using StoreApp.Foundation.Views.Factories;
+
+namespace StoreApp.Core.Views.Factories
+{
+    public class BookViewFactory : IBookViewFactory
+    {
+        private readonly Func<BookView> createView;
+
+        public BookViewFactory(Func<BookView> createView )
+        {
+            this.createView = createView;
+        }
+
+        public FrameworkElement Create()
+        {
+            return createView();
+        }
+    }
+}
