30c7c08ae458d46e4c5c2372054a9f523eb1b3ed
Philip Trevino <philt5252@gmail.com>
2015-01-29 20:54:45 -0600

    dashboard view in region

---
+++ StoreApp/StoreApp.Core/Controllers/DashboardController.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using Microsoft.Practices.Prism.Regions;
+using StoreApp.Foundation.Controllers;
+using StoreApp.Foundation.Views;
+using StoreApp.Foundation.Views.Factories;
+
+namespace StoreApp.Core.Controllers
+{
+    public class DashboardController : IDashboardController
+    {
+        private readonly IDashboardViewFactory dashboardViewFactory;
+        private readonly IRegionManager regionManager;
+
+        public DashboardController(IDashboardViewFactory dashboardViewFactory,
+            IRegionManager regionManager)
+        {
+            this.dashboardViewFactory = dashboardViewFactory;
+            this.regionManager = regionManager;
+        }
+
+        public void Show()
+        {
+            var dashboardView = dashboardViewFactory.Create();
+
+            regionManager.Regions[Regions.MainRegion].Add(dashboardView);
+            regionManager.Regions[Regions.MainRegion].Activate(dashboardView);
+        }
+    }
+}
--- StoreApp/StoreApp.Core/ViewModels/MenuItems/PerformanceMenuItemViewModel.cs
+++ StoreApp/StoreApp.Core/ViewModels/MenuItems/PerformanceMenuItemViewModel.cs
@@ -7,27 +7,30 @@ using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Events;
+using StoreApp.Foundation.Controllers;
 using StoreApp.Foundation.ViewModels;
 
 namespace StoreApp.Core.ViewModels.MenuItems
 {
     public class PerformanceMenuItemViewModel : MenuItemViewModel
     {
+        private readonly IDashboardController dashboardController;
         public override string Text { get { return "Performance"; } }
         public override ICommand MenuCommand { get; protected set; }
 
         public override String NewText { get { return "P"; } }
         public override BitmapImage Image { get { return new BitmapImage(new Uri("Images/menu.png", UriKind.Relative)); } }
 
-        public PerformanceMenuItemViewModel(IEventAggregator eventAggregator)
+        public PerformanceMenuItemViewModel(IEventAggregator eventAggregator, IDashboardController dashboardController)
             :base(eventAggregator)
         {
+            this.dashboardController = dashboardController;
             MenuCommand = new DelegateCommand(ExecuteMenuCommand);
         }
 
         private void ExecuteMenuCommand()
         {
-
+            dashboardController.Show();
         }
     }
 }
