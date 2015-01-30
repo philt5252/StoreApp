0276ffb7fa79300189eeaa2cee4cc37db950c926
mregan59 <mregan59@gmail.com>
2015-01-29 20:36:55 -0600

    widget factories and views

---
+++ StoreApp/StoreApp.Core.Views/Factories/SampleWidgetViewFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using StoreApp.Core.Views.Views;
+using StoreApp.Foundation.Views.Factories;
+
+namespace StoreApp.Core.Views.Factories
+{
+    public class SampleWidgetViewFactory : ISampleWidgetViewFactory
+    {
+        private readonly Func<SampleWidgetView> createView;
+
+        public SampleWidgetViewFactory(Func<SampleWidgetView> createView )
+        {
+            this.createView = createView;
+        }
+
+        public FrameworkElement Create()
+        {
+            return createView();
+        }
+    }
+}
