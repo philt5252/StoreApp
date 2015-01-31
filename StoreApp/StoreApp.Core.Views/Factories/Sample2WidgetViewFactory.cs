2b425b64485696d5b0435b878a9bc96eaf6071ca
mregan59 <mregan59@gmail.com>
2015-01-30 22:07:46 -0600

    sample2 column chart

--- StoreApp/StoreApp.Core.Views/Factories/Sample2WidgetViewFactory.cs
+++ StoreApp/StoreApp.Core.Views/Factories/Sample2WidgetViewFactory.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using StoreApp.Core.Views.Views;
+using StoreApp.Foundation.Views.Factories;
 
 namespace StoreApp.Core.Views.Factories
 {
-    class Sample2WidgetViewFactory
+    public class Sample2WidgetViewFactory : ISample2WidgetViewFactory
     {
+             private readonly Func<Sample2WidgetView> createView;
+
+        public Sample2WidgetViewFactory(Func<Sample2WidgetView> createView)
+        {
+            this.createView = createView;
+        }
+
+        public Control Create()
+        {
+            return createView();
+        }
     }
 }
--- StoreApp/StoreApp.Foundation.Views/Factories/ISample2WidgetViewFactory.cs
+++ StoreApp/StoreApp.Foundation.Views/Factories/ISample2WidgetViewFactory.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace StoreApp.Foundation.Views.Factories
 {
-    interface ISample2WidgetViewFactory
+    public interface ISample2WidgetViewFactory
     {
+        Control Create();
     }
 }
