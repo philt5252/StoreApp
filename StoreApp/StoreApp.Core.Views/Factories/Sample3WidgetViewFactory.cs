ec5021174268cd3a58d0e2b977c4e4b08d25737c
mregan59 <mregan59@gmail.com>
2015-01-31 15:28:22 -0600

    factories

--- StoreApp/StoreApp.Core.Views/Factories/Sample3WidgetViewFactory.cs
+++ StoreApp/StoreApp.Core.Views/Factories/Sample3WidgetViewFactory.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using StoreApp.Core.Views.Views;
+using StoreApp.Foundation.Views.Factories;
 
 namespace StoreApp.Core.Views.Factories
 {
-    class SampleWidget3ViewFactory
+    public class Sample3WidgetViewFactory : ISample3WidgetViewFactory
     {
+        private readonly Func<Sample3WidgetView> createView;
+
+        public Sample3WidgetViewFactory(Func<Sample3WidgetView> createView )
+        {
+            this.createView = createView;
+        }
+
+        public Control Create()
+        {
+            return createView();
+        }
     }
 }
--- StoreApp/StoreApp.Foundation.Views/Factories/ISample3WidgetViewFactory.cs
+++ StoreApp/StoreApp.Foundation.Views/Factories/ISample3WidgetViewFactory.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace StoreApp.Foundation.Views.Factories
 {
-    interface ISample3WidgetViewFactory
+    public interface ISample3WidgetViewFactory
     {
+        Control Create();
     }
 }
