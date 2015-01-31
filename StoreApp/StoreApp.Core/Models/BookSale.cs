32622239625a610db2eb74831fbae69bf4e67425
Philip Trevino <philt5252@gmail.com>
2015-01-30 22:14:00 -0600

    sample2 widget

--- StoreApp/StoreApp.Core/Models/BookSale.cs
+++ StoreApp/StoreApp.Core/Models/BookSale.cs
@@ -1,7 +1,10 @@
-﻿namespace StoreApp.Core.Models
+﻿using System;
+
+namespace StoreApp.Core.Models
 {
     public class BookSale
     {
+        public DateTime Date { get; set; }
         public string Genre { get; set; }
         public int Sales { get; set; }
     }
---
+++ StoreApp/StoreApp.Core/ViewModels/Sample2WidgetViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+using System.Windows.Controls;
+using StoreApp.Core.Models;
+using StoreApp.Foundation.ViewModels;
+using StoreApp.Foundation.Views.Factories;
+
+namespace StoreApp.Core.ViewModels
+{
+    public class Sample2WidgetViewModel :WidgetViewModel
+    {
+        private readonly ISample2WidgetViewFactory sample2WidgetViewFactory;
+
+        public override string WidgetName
+        {
+            get { return "Weekly Sales"; }
+        }
+
+        public override Bitmap Image
+        {
+            get { return null; }
+        }
+
+        public BookSale[] BookSales { get; protected set; }
+
+        public Sample2WidgetViewModel(ISample2WidgetViewFactory sample2WidgetViewFactory)
+        {
+            this.sample2WidgetViewFactory = sample2WidgetViewFactory;
+
+            BookSales = new[]
+            {
+                new BookSale {Genre = "Monday", Sales = 20},
+                new BookSale {Genre = "Tuesday", Sales = 7},
+                new BookSale {Genre = "Wednesday", Sales = 10},
+                new BookSale {Genre = "Thursday", Sales = 15},
+                new BookSale {Genre = "Friday", Sales = 12},
+            };
+        }
+
+        public override Control CreateWidget()
+        {
+            var view = sample2WidgetViewFactory.Create();
+            view.DataContext = CreateWidgetViewModel();
+
+            return view;
+        }
+
+        protected override IWidgetViewModel CreateWidgetViewModel()
+        {
+            return new Sample2WidgetViewModel(sample2WidgetViewFactory);
+        }
+    }
+}
